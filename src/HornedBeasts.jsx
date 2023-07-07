cf2693c96c35a254170d844e0b87d70fb655ad0d
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-07-07 02:29:05 +0000

    added hornedbeasts data

---
+++ src/HornedBeasts.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+    render() {
+        return (
+            <div>
+                <img alt="uniwhal" src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" />
+                <h2>{UniWhal}</h2>
+                <p>"A unicorn and narwhal touching horns</p>
+            </div>
+        )
+    };
+}
+class HornedBeasts extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{Rhino}</h2>
+                <p>"Rhino family"</p>
+                <img alt="rhino" src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" />
+            </div>
+        )
+    };
+}
+class HornedBeasts extends React.Component {
+    render() {
+        return(
+            <div>
+            <img alt="unicorn" src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" />
+            <h2>{Unicorn}</h2>
+            <p>"Silly unicorn hat"</p>
+        </div>
+        )
+    }
+}
